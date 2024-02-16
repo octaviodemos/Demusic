74047eabf3c42b06090880f6ff057711bf7b3c0c
Octávio Demos <octavio_demos@hotmail.com>
2024-02-16 11:06:16 -0300

    Add project files.

---
+++ Demusic/LoginView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TelaDemusic
+{
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+            LoginBox.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("teste");
+        }
+
+        private void AcessaButton(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Teste");
+        }
+
+    }
+}
---
+++ Demusic/Modelos/Usuarios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelaDemusic.Modelos
+{
+    public class Usuarios
+    {
+        private int _id;
+        private string _login;
+        private string _password;
+
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
