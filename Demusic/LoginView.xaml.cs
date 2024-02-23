1acde8bbdd95ee28136e4738d05274f26bd67ea6
Octávio Demos <octavio_demos@hotmail.com>
2024-02-23 18:00:58 -0300

    validalogin

--- Demusic/LoginView.xaml.cs
+++ Demusic/LoginView.xaml.cs
@@ -9,28 +9,76 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Demusic;
+using Demusic.Modelos;
+using Demusic.Repositorios;
 
 namespace Demusic
 {
     public partial class LoginView : Window
     {
+        UsuariosRepositorio usuariosRepositorio;
         public LoginView()
         {
             InitializeComponent();
             LoginBox.Focus();
-        }
-
-        private void CadastroButton(object sender, RoutedEventArgs e)
-        {
-            CadastroView TelaCadastro = new CadastroView();
-            TelaCadastro.Show();
-
+            usuariosRepositorio = new UsuariosRepositorio();
         }
 
         private void AcessaButton(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("teste");
-        }
+          string LoginUsuario = LoginBox.Text;
+          string SenhaUsuario = SenhaBox.Password;
 
+          Usuarios usuario = new Usuarios();
+            {
+                LoginUsuario = LoginBox.Text;
+                SenhaUsuario = SenhaBox.Password;
+            }
+
+          if (string.IsNullOrEmpty(LoginUsuario))
+            {
+                MessageBox.Show("Digite seu login");
+                LoginBox.Focus();
+                return;
+            }
+          if (string.IsNullOrEmpty (SenhaUsuario))
+            {
+                MessageBox.Show("Digite a sua senha");
+                SenhaBox.Focus();
+                return;
+            }
+
+          bool ExibeLogin = usuariosRepositorio.ValidaLogin(usuario);
+          if (ExibeLogin)
+          {
+             MessageBox.Show("Você possui login");
+          }
+          else
+          {
+             MessageBox.Show("Você não possui login");
+          }
+
+        
+
+        }
+        private void CadastroButton(object sender, RoutedEventArgs e)
+        {
+            CadastroView TelaCadastro = new CadastroView();
+            TelaCadastro.Show();
+        }
+        private void SenhaBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                AcessaButton(null, null);
+            }
+        }
+        private void LoginBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SenhaBox.Focus();
+            }
+        }
     }
 }
--- Demusic/Repositorios/UsuariosRepositorio.cs
+++ Demusic/Repositorios/UsuariosRepositorio.cs
@@ -5,9 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Demusic.Modelos;
+using System.Reflection.PortableExecutable;
+using System.Data;
 
 namespace Demusic.Repositorios
-{
+{ 
     public class UsuariosRepositorio
     {
         private const string ConnectionString = "Data Source=localhost;Initial Catalog=Demusics;Integrated Security=False;User Id=sa;Password=super990025;";
@@ -19,21 +21,42 @@ namespace Demusic.Repositorios
                 conn.Open();
                 SqlCommand command = conn.CreateCommand();
                 string sql = $@"
-INSERT INTO [dbo].[Usuario]
-           ([Login]
-           ,[Senha]
-           ,[Nome]
-           ,[Email])
-     VALUES
-           ('{usuarios.Login}'
-           ,'{usuarios.Password}'
-           ,'{usuarios.Nome}'
-           ,'{usuarios.Email}')
-        ";
+        INSERT INTO [dbo].[Usuario]
+                   ([Login]
+                   ,[Senha]
+                   ,[Nome]
+                   ,[Email])
+        VALUES
+               ('{usuarios.Login}'
+               ,'{usuarios.Password}'
+               ,'{usuarios.Nome}'
+               ,'{usuarios.Email}')
+            ";
                 command.CommandText = sql; 
                 int qtd = command.ExecuteNonQuery(); 
                 Console.WriteLine("Linhas afetadas:" + qtd);
             }
         }
+
+        public bool ValidaLogin(Usuarios usuarios)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand command = conn.CreateCommand();
+                string sql = $@"
+        SELECT  Id
+                ,Login
+                ,Senha
+        FROM Usuario
+        WHERE Login = '{usuarios.Login}' and Senha = '{usuarios.Password}';
+        ";
+                command.CommandText = sql;
+                SqlDataReader reader = command.ExecuteReader();
+
+                boolo Existe
+            }
+        }
     }
 }
+
